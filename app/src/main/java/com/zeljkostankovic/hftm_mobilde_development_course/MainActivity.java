286cfb24c414316b435fb8cea660f185281c3adb
Zeljko Stankovic <zeljko.stankovic@hftm.ch>
2022-05-27 14:53:10 +0200

    init Project

---
+++ app/src/main/java/com/zeljkostankovic/hftm_mobilde_development_course/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zeljkostankovic.hftm_mobilde_development_course;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
