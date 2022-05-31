b0cdd4ec73054480aceea39dd5e3404c6f97febd
Zeljko Stankovic <zeljko.stankovic@hftm.ch>
2022-05-31 21:51:35 +0200

    abstract intent task done

--- app/src/main/java/com/zeljkostankovic/hftm_mobilde_development_course/MainActivity.java
+++ app/src/main/java/com/zeljkostankovic/hftm_mobilde_development_course/MainActivity.java
@@ -2,7 +2,15 @@ package com.zeljkostankovic.hftm_mobilde_development_course;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent abstractIntend = new Intent();
+        abstractIntend.setAction(Intent.ACTION_SEND);
+        abstractIntend.putExtra(Intent.EXTRA_TEXT,"Text von abstraktem Intent");
+        abstractIntend.setType("text/plain");
+
+        if(abstractIntend.resolveActivity(getPackageManager()) != null) {
+            startActivity(abstractIntend);
+        }
     }
+
+
+
 }
